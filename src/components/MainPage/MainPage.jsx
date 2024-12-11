4bcc606c08a824b87582ccbdc64473e1c4c2ed61
Andriian Siromskyi <157056772+Andriian-Siromskyi@users.noreply.github.com>
2024-12-11 21:18:59 +0200

    main verst

---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,49 @@
+import "./MainPage.module.css";
+
+const MainPage = () => {
+  return (
+    <div className="main-page">
+      <header className="main-paige-header">
+        <div className="main-paige-logo">
+          <img src="" alt="Tracker of Water" />
+        </div>
+        <button className="main-signin-button">Sign in</button>
+      </header>
+      <main className="main-paige-content">
+        <section className="main-paige-info">
+          <h1>Water consumption tracker</h1>
+          <p>Record daily water intake and track</p>
+          <div className="main-paige-benefits">
+            <h2>Tracker Benefits</h2>
+            <ul>
+              <li>Habit drive</li>
+              <li>View statistics</li>
+              <li>Personal rate setting</li>
+            </ul>
+          </div>
+          <button className="try-main-button">Try tracker</button>
+        </section>
+        <section className="why-drink-water">
+          <h2>Why drink water</h2>
+          <ul>
+            <li>Supply of nutrients to all organs</li>
+            <li>Providing oxygen to the lungs</li>
+            <li>Maintaining the work of the heart</li>
+            <li>Release of processed substances</li>
+            <li>Ensuring the stability of the internal environment</li>
+            <li>Maintaining within the normal temperature</li>
+            <li>Maintaining an immune system capable of resisting disease</li>
+          </ul>
+        </section>
+        <div className="main-paige-illustration">
+          <img
+            src=
+            alt=
+          />
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default MainPage;
